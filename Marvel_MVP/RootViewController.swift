dc82a369ed88d93caca814dcfbfc1c1ad806e6e9
Andres <phdafoe@gmail.com>
2020-01-08 17:24:29 +0100

    Initial Commit

---
+++ Marvel_MVP/RootViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RootViewController.swift
+//  Marvel_MVP
+//
+//  Created by Andres Felipe Ocampo Eljaiesk on 08/01/2020.
+//  Copyright © 2020 icologic. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
